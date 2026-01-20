8e2aac5ddb4d444a387a26f97f09d66638d36da1
IceyFoxes <zhanglikevinofficial@gmail.com>
2026-01-21 03:43:59 +0800

    Add TaskType enum to centralize task type
    - Create TaskType enum with TODO, DEADLINE, and EVENT constants
    - Centralize type symbols in enum for easier maintenance

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,14 +3,14 @@ public class Event extends Task {
     protected String by;
 
     public Event(String description, String from, String by) {
-        super(description);
+        super(TaskType.EVENT, description);
         this.from = from;
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + by + ")";
+        return super.toString() + " (from: " + from + " to: " + by + ")";
     }
 
 }
